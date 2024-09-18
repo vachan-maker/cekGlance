384de92a49351a2808dea4ed9c1a40f322beecb5
vachan-maker <vachan@tutanota.com>
2024-09-18 12:11:33 +0530

    flex container and card component testing

--- src/routes/past-events/+page.svelte
+++ src/routes/past-events/+page.svelte
@@ -2,6 +2,18 @@
 	import Card from "./card.svelte";
 
 </script>
-<div class="container">
+<div  class="space-y-4 flex flex-col gap-9">
+    <Card/>
+    <Card/>
+    <Card/>
+    <Card/>
+    <Card/>
+    <Card/>
+    <Card/>
+    <Card/>
+    <Card/>
+    <Card/>
+    <Card/>
+    <Card/>
     <Card/>
 </div>
--- src/routes/past-events/card.svelte
+++ src/routes/past-events/card.svelte
@@ -1,14 +1,17 @@
 <script>
-    import { Card, Button, Toggle } from 'flowbite-svelte';
-    import { ArrowRightOutline } from 'flowbite-svelte-icons';
-  </script>
-  
-  <div  class="space-y-4">
-    <Card class="m-auto" img="/images/image-1.webp">
-      <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">College Graduation Ceremony</h5>
-      <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-      <Button>
-        Read more <ArrowRightOutline class="w-6 h-6 ms-2 text-white" />
-      </Button>
-    </Card>
-  </div>
+	import { Card, Button, Toggle } from 'flowbite-svelte';
+	import { ArrowRightOutline } from 'flowbite-svelte-icons';
+</script>
+
+<Card
+	class="m-auto"
+	img="https://lh5.googleusercontent.com/p/AF1QipN-neC8AguRYsGnYVjnj-T5tvHJBEoecZ6Fa_Qz=w400-h224-k-no"
+>
+	<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+		College Graduation Ceremony
+	</h5>
+	<p class="mb-3 font-normal leading-tight text-gray-700 dark:text-gray-400">
+		Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological
+		order.
+	</p>
+</Card>
