366c6cd7b4d40e28acfd349765c8e45aef81c219
vachan-maker <vachan@tutanota.com>
2024-09-18 20:55:24 +0530

    Fixed up card component working....i think. and also loaded data from supabase

--- src/routes/past-events/+page.svelte
+++ src/routes/past-events/+page.svelte
@@ -1,9 +1,11 @@
 <script>
 	import Card from "./card.svelte";
     export let data;
-    const {past_events} = data;
+    const past_events= data.past_events;
 
 </script>
 <div  class="space-y-4 flex flex-col gap-9">
-    <Card href={"image.webp"}/>
+    {#each past_events as event}
+    <Card href="https://xnpuqvymwwzjiwycsabx.supabase.co/storage/v1/object/public/cekStorage/{event.imagePath}" heading={event.title} text={event.description}/>
+    {/each}
 </div>
--- src/routes/past-events/card.svelte
+++ src/routes/past-events/card.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import { Card, Button, Toggle } from 'flowbite-svelte';
 	export let href:string = '';
+	export let heading:String='';
+	export let text:String='';
 
 </script>
 
@@ -8,11 +10,8 @@
 	class="m-auto"
 	img="{href}"
 >
-	<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-		College Graduation Ceremony
+	<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{heading}
 	</h5>
-	<p class="mb-3 font-normal leading-tight text-gray-700 dark:text-gray-400">
-		Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological
-		order.
+	<p class="mb-3 font-normal leading-tight text-gray-700 dark:text-gray-400">{text}
 	</p>
 </Card>
