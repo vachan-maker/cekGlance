528fe6cd68341107905413e9b8b9829ffa0356e5
vachan-maker <vachan@tutanota.com>
2024-09-22 20:13:53 +0530

    active nav link

--- src/routes/navbar.svelte
+++ src/routes/navbar.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
     import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
+    import { page } from '$app/stores';
+    $: activeUrl = $page.url.pathname;
   </script>
-  <Navbar  >
+  <Navbar>
     <NavBrand href="/">
       <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">CEK Glance</span>
     </NavBrand>
     <NavHamburger  />
-    <NavUl >
+    <NavUl {activeUrl} >
       <NavLi href="/upcoming" data-sveltekit-preload-data="hover">Next Up</NavLi>
       <NavLi href="/past-events" data-sveltekit-preload-data="hover">Looking Back</NavLi>
       <NavLi href="/about" data-sveltekit-preload-data="hover">Updates</NavLi>
