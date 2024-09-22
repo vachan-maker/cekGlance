65cc4cae573d4e531510191731094c42915b62f9
vachan-maker <vachan@tutanota.com>
2024-09-22 19:55:21 +0530

    added register button

--- src/routes/card.svelte
+++ src/routes/card.svelte
@@ -7,6 +7,7 @@
 	export let heading:string='';
 	export let text:string='';
 	export let tags:string[];
+	export let link:string='';
 
 </script>
 
@@ -16,9 +17,17 @@
 >
 	<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{heading}
 	</h5>
-	<p class="mb-3 font-normal leading-tight text-gray-700 dark:text-gray-400">{text}
-	</p>
+	{#if tags}
 	{#each tags as tag}
 	<Badge large>{tag}</Badge>
 	{/each}
+	{/if}
+	{#if link}
+	<br />
+	<p class="mb-3 font-normal leading-tight text-gray-700 dark:text-gray-400">{text}
+	</p>
+	<Button href={link}>
+		Register Now
+	  </Button>
+	{/if}
 </Card>
--- src/routes/upcoming/+page.svelte
+++ src/routes/upcoming/+page.svelte
@@ -6,6 +6,6 @@
 </script>
 <div  class="space-y-4 flex flex-col gap-9">
     {#each new_events as event}
-    <Card href="https://xnpuqvymwwzjiwycsabx.supabase.co/storage/v1/object/public/cekStorage/Upcoming/{event.imagePath}" heading={event.title} text={event.description} tags={event.tags}/>
+    <Card href="https://xnpuqvymwwzjiwycsabx.supabase.co/storage/v1/object/public/cekStorage/Upcoming/{event.imagePath}" heading={event.title} text={event.description} tags={event.tags} link={event.link}/>
     {/each}
 </div>
