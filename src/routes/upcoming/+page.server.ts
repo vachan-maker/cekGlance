6e921618e7902253a841809c880e1713325f0851
vachan-maker <vachan@tutanota.com>
2024-09-23 20:08:28 +0530

    added tabs

--- src/routes/upcoming/+page.server.ts
+++ src/routes/upcoming/+page.server.ts
@@ -4,9 +4,11 @@ import { error } from "@sveltejs/kit";
 let today:string = new Date().toISOString().slice(0,10);
 console.log(today);
 export async function load() {
-    const { data } = await supabase.from("Upcoming Events").select('*').gte('date', `>= ${today}`).order('date',{ascending:true});
-    console.log(data);
+    const { data } = await supabase.from("Upcoming Events").select('*').gte('date', `${today}`).order('date',{ascending:true});
+    const completed_events =  (await supabase.from("Upcoming Events").select('*').lt('date', `${today}`).order('date',{ascending:true})).data;
+    console.log(completed_events);
     return {
       new_events: data ?? [],
+      completed_events:completed_events ?? [],
     };
   }
--- src/routes/upcoming/+page.svelte
+++ src/routes/upcoming/+page.svelte
@@ -1,13 +1,42 @@
 <script>
 	import { Heading } from 'flowbite-svelte';
-	import Card from '../card.svelte';
-    export let data;
-    const new_events= data.new_events;
 
+	import { Tabs, TabItem } from 'flowbite-svelte';
+	import Card from '../card.svelte';
+	export let data;
+	const new_events = data.new_events;
+    const completed_events = data.completed_events;
 </script>
-<Heading tag="h2" class="text-center mb-4">Upcoming Events</Heading>
-<div  class="space-y-4 flex flex-col gap-9">
-    {#each new_events as event}
-    <Card href="https://xnpuqvymwwzjiwycsabx.supabase.co/storage/v1/object/public/cekStorage/Upcoming/{event.imagePath}" heading={event.title} text={event.description} tags={event.tags} link={event.link}/>
-    {/each}
-</div>
+
+<Tabs tabStyle="full" defaultClass="flex rounded-lg divide-x rtl:divide-x-reverse divide-gray-200 shadow dark:divide-gray-700">
+    <TabItem class="w-full" open>
+      <span slot="title">Upcoming Events</span>
+      <Heading tag="h2" class="mb-4 text-center">Upcoming Events</Heading>
+      <div class="flex flex-col gap-9 space-y-4">
+        {#each new_events as event}
+            <Card
+                href="https://xnpuqvymwwzjiwycsabx.supabase.co/storage/v1/object/public/cekStorage/Upcoming/{event.imagePath}"
+                heading={event.title}
+                text={event.description}
+                tags={event.tags}
+                link={event.link}
+            />
+        {/each}
+    </div>
+    </TabItem>
+    <TabItem class="w-full">
+      <span slot="title">Completed Events</span>
+      <Heading tag="h2" class="mb-4 text-center">Completed Events</Heading>
+      <div class="flex flex-col gap-9 space-y-4">
+        {#each completed_events as complete}
+            <Card
+                href="https://xnpuqvymwwzjiwycsabx.supabase.co/storage/v1/object/public/cekStorage/Upcoming/{complete.imagePath}"
+                heading={complete.title}
+                text={complete.description}
+                tags={complete.tags}
+                link={complete.link}
+            />
+        {/each}
+    </div>
+    </TabItem>
+  </Tabs>
