d15a19bf726279cd8cda176392880f0da4b185ba
vachan-maker <vachan@tutanota.com>
2024-10-02 18:07:46 +0530

    tried to handle situation when there are no upcoming events

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,23 +6,29 @@
   const past_events = data.past_events;
   </script>
   
-  <div class="p-20 bg-slate-100">
+  <div class="pl-16 py-28 bg-slate-100">
     <Heading tag="h1">Browse what's happening at CEK</Heading>
   </div>
-  <div class="p-12">
-  <Heading tag="h2" class="text-center"><A href="/upcoming">Upcoming Events</A></Heading>
+  <div class="pl-16">
+  <Heading tag="h2"><A href="/upcoming">Upcoming Events</A></Heading>
   </div>
   <p></p>
-  <div class="flex flex-wrap justify-center">
+  {#if new_events}
+  <div class="flex flex-wrap pl-16 py-20">
     {#each new_events as event}
     <FrontCard href="/events/{event.identifier}" img="https://xnpuqvymwwzjiwycsabx.supabase.co/storage/v1/object/public/cekStorage/Upcoming/{event.imagePath}" heading={event.title} tags={event.tags} date={event.date} link={event.link}/>
     {/each}
   </div>
-<div class="p-12">
-  <Heading tag="h2" class="text-center"><A href="/past-events">Past Events</A></Heading>
+  {:else}
+  <div class="">
+    <Heading tag="h3">No Upcoming Events!</Heading>
+  </div>
+  {/if}
+<div class="pl-16">
+  <Heading tag="h2"><A href="/past-events">Past Events</A></Heading>
 </div>
 <p></p>
-<div class="flex flex-wrap justify-center">
+<div class="flex flex-wrap pl-16">
   {#each past_events as past_event}
   <FrontCard img="https://xnpuqvymwwzjiwycsabx.supabase.co/storage/v1/object/public/cekStorage/{past_event.imagePath}" heading={past_event.title} tags={past_event.tags} text={past_event.description}/>
   {/each}
