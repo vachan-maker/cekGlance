9f08d22dadd62f11f7701a773b930bd349860e10
vachan-maker <vachan@tutanota.com>
2024-10-02 10:43:44 +0530

    dynamic routing sort of works...yay!

--- src/routes/organizers/[slug]/+page.svelte
+++ src/routes/organizers/[slug]/+page.svelte
@@ -1,8 +1,11 @@
 <script>
-	import { Heading } from "flowbite-svelte";
+	import { Heading, P } from "flowbite-svelte";
     export let data;
-    const organizer = data.organizer;
+    const organizer = data.organizer[0];
 </script>
 <div class="p-20 bg-slate-100">
     <Heading tag="h1">{organizer.name}</Heading>
+    {#if organizer.description}
+    <P>{organizer.description}</P>
+    {/if}
   </div>
--- src/routes/past-events/+page.server.ts
+++ src/routes/past-events/+page.server.ts
@@ -1,6 +1,4 @@
-import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_URL } from "$env/static/public";
 import { supabase } from "$lib/supabaseClient";
-import { error } from "@sveltejs/kit";
 export async function load() {
     const { data } = await supabase.from("cek-past-events").select('*').order('date',{ascending:false});
     console.log(data);
