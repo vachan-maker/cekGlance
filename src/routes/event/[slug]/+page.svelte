925d3b6c371315e532c2b18c6874c18f0686d9f6
vachan-maker <vachan@tutanota.com>
2024-10-02 15:35:47 +0530

    event page so pretty..hehe

--- src/routes/event/[slug]/+page.svelte
+++ src/routes/event/[slug]/+page.svelte
@@ -1,15 +1,47 @@
 <script>
-	import { Heading, P } from "flowbite-svelte";
-
+	import { Heading, Listgroup, ListgroupItem, P,Badge, Tooltip, Button } from "flowbite-svelte";
+    import { ArrowUpRightFromSquareSolid } from "flowbite-svelte-icons";
     export let data;
     const event= data.event[0];
 </script>
-<div class="flex flex-col items-center gap-2 p-8 bg-slate-100 lg:flex-row lg:items-start ">
+<div class="flex flex-col items-center gap-2 p-8 bg-slate-100 lg:flex-row lg:items-start">
     <div class="flex m-7">
         <img src="https://xnpuqvymwwzjiwycsabx.supabase.co/storage/v1/object/public/cekStorage/Upcoming/{event.imagePath}" alt="" class="w-96 aspect-square"/>
     </div>
-    <div class="flex flex-col flex-1">
+    <div class="flex flex-col flex-1 mt-8 justify-center">
         <Heading tag="h1" class="mb-6">{event.title}</Heading>
-        <P>{event.description}</P>
+        <P class="mb-2">{event.description}</P>
+        {#if event.link}
+        <Button size="lg" class="max-w-32 my-5" href="{event.link}"><ArrowUpRightFromSquareSolid class="w-5 h-5 me-2" />Register</Button>
+        {/if}
+        <Listgroup class="max-w-lg">
+            <ListgroupItem class="text-base font-semibold gap-2 flex flex-row justify-between">
+                <P>Organised By:</P>
+                <div>
+                    {#each event.tags as tag}
+                    <Badge large href="/organizers/{tag}">{tag}</Badge>
+                    {/each}
+                </div>
+            </ListgroupItem>
+            <ListgroupItem class="text-base font-semibold gap-2 flex flex-row justify-between">
+                <P>Date:</P>
+                <P>{event.date}</P>
+            </ListgroupItem>
+            {#if event.mode}
+            <ListgroupItem class="text-base font-semibold gap-2 flex flex-row justify-between">
+                <P>Mode:</P>
+                <Badge large color="pink" id="{event.mode}">{event.mode}</Badge>
+                {#if event.mode == 'Hybrid'}
+                <Tooltip triggeredBy="#Hybrid">Event is held both online and on campus</Tooltip>
+                {/if}
+            </ListgroupItem>
+            {/if}
+            {#if event.venue}
+            <ListgroupItem class="text-base font-semibold gap-2 flex flex-row justify-between">
+                <P>Venue:</P>
+                <Badge large color="red">{event.venue}</Badge>
+            </ListgroupItem>
+            {/if}
+          </Listgroup>
     </div>
 </div>
