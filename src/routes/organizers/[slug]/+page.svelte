c16c97cfc521f1fdf99f99d514ee56c5b863c720
vachan-maker <vachan@tutanota.com>
2024-10-02 18:48:54 +0530

    added support for organization logos

--- src/routes/organizers/[slug]/+page.svelte
+++ src/routes/organizers/[slug]/+page.svelte
@@ -1,15 +1,18 @@
 <script>
-	import { Heading, P, A } from "flowbite-svelte";
+	import { Heading, P, A,Avatar } from "flowbite-svelte";
 	import FrontCard from "../../front-card.svelte";
     export let data;
     const organizer = data.organizer[0];
     const events = data.events;
 </script>
-<div class="p-20 bg-slate-100">
-    <Heading tag="h1">{organizer.name}</Heading>
+<div class="p-2 bg-slate-100 flex flex-col justify-center items-center lg:flex-row lg:p-16">
+  <Avatar src="https://xnpuqvymwwzjiwycsabx.supabase.co/storage/v1/object/public/cekStorage/Logos/{organizer.logo}" rounded size="xl" />
+  <div class="flex flex-col mx-8">  
+  <Heading tag="h1" class="my-2">{organizer.name}</Heading>
     {#if organizer.description}
     <P>{organizer.description}</P>
     {/if}
+  </div>
   </div>
   <div class="p-12">
     <Heading tag="h2" class="text-center">Hosted Events</Heading>
