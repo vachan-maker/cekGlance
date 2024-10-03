96f67956e8f04ca32dc708c371a2db3450768664
vachan-maker <vachan@tutanota.com>
2024-10-03 18:37:05 +0530

    Fixed upcoming page so that it won't be displayed if there are no events

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -2,11 +2,13 @@ import { supabase } from "$lib/supabaseClient";
 let today:string = new Date().toISOString().slice(0,10);
 console.log(today);
 export async function load() {
-    const { data } = await supabase.from("Upcoming Events").select('*').order('date',{ascending:true});
+    const { data } = (await supabase.from("Upcoming Events").select('*').lt('date', `${today}`).order('date',{ascending:false}));
+    const upcoming_events = (await supabase.from("Upcoming Events").select('*').gte('date', `${today}`).order('date',{ascending:true})).data;
     const past_events = (await supabase.from("cek-past-events").select('*').order('date',{ascending:false})).data;
     console.log(data);
     return {
-      new_events: data ?? [],
+      archive: data ?? [],
       past_events:past_events ?? [],
+      upcoming_events: upcoming_events ?? [],
     };
   }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,13 +3,27 @@
    import { ThumbsUpSolid, ArrowRightOutline } from 'flowbite-svelte-icons'
 	import FrontCard from './front-card.svelte';
   export let data;
-  const new_events= data.new_events;
+  const archive= data.archive;
   const past_events = data.past_events;
+  const upcoming_events = data.upcoming_events;
   </script>
   
   <div class="p-10 min-h-64 bg-slate-100 flex flex-col justify-center items-center">
-    <Heading tag="h1" class="text-center">Browse what's happening at CEK</Heading>
+    <Heading tag="h1" class="text-center">Stay in the Loop with CEK Glance!</Heading>
   </div>
+      {#if upcoming_events.length != 0}
+  <div class="pt-6 flex justify-center items-center">
+    <div class="container">
+    <Heading tag="h3">Upcoming Events</Heading>
+    </div>
+    <Button pill={true} class="!p-2" href="/upcoming"><ArrowRightOutline class="w-6 h-6"/></Button>
+    </div>
+    <div class="flex flex-row overflow-x-auto snap-x snap-mandatory lg:grid lg:grid-cols-4 gap-8 lg:auto-rows-auto lg:grid-flow-row-dense py-6">
+      {#each upcoming_events as upcoming}
+      <FrontCard href="/events/{upcoming.identifier}" img="https://xnpuqvymwwzjiwycsabx.supabase.co/storage/v1/object/public/cekStorage/Upcoming/{upcoming.imagePath}" heading={upcoming.title} tags={upcoming.tags} date={upcoming.date} link={upcoming.link}/>
+      {/each}
+    </div>
+    {/if}
   <div class="pt-6 flex justify-center items-center">
   <div class="container">
   <Heading tag="h3">From the Archive</Heading>
@@ -17,17 +31,11 @@
   </div>
   <Button pill={true} class="!p-2" href="/archive"><ArrowRightOutline class="w-6 h-6"/></Button>
   </div>
-  {#if new_events}
   <div class="flex flex-row overflow-x-auto snap-x snap-mandatory lg:grid lg:grid-cols-4 gap-8 lg:auto-rows-auto lg:grid-flow-row-dense py-6">
-    {#each new_events as event}
+    {#each archive as event}
     <FrontCard href="/events/{event.identifier}" img="https://xnpuqvymwwzjiwycsabx.supabase.co/storage/v1/object/public/cekStorage/Upcoming/{event.imagePath}" heading={event.title} tags={event.tags} date={event.date} link={event.link}/>
     {/each}
   </div>
-  {:else}
-  <div class="">
-    <Heading tag="h3">No Upcoming Events!</Heading>
-  </div>
-  {/if}
 <div class="">
   <Heading tag="h2"><A href="/past-events">Past Events</A></Heading>
 </div>
