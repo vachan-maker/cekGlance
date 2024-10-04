51f73b76454404e5dc5de948bf37ead078a0211b
vachan-maker <vachan@tutanota.com>
2024-10-04 07:45:35 +0530

    fixed organizations page..use avatar and grid

--- src/routes/organizers/+page.svelte
+++ src/routes/organizers/+page.svelte
@@ -1,18 +1,19 @@
 <script>
-	import { Heading } from "flowbite-svelte";
-	import Card from "../card.svelte";
-	import FrontCard from "../front-card.svelte";
-    export let data;
-    const organizers = data.organizer;
-    console.log(organizers);
+	import { Heading, Avatar,P } from 'flowbite-svelte';
+	import Card from '../card.svelte';
+	import FrontCard from '../front-card.svelte';
+	export let data;
+	const organizers = data.organizer;
+	console.log(organizers);
 </script>
-    <Heading tag="h2" class="mb-4 text-center">Completed Events</Heading>
-            <div class="flex flex-row flex-wrap gap-4 justify-start align-baseline lg:grid lg:grid-cols-3">
-                {#each organizers as org}
-                    <Card
-                        img="https://xnpuqvymwwzjiwycsabx.supabase.co/storage/v1/object/public/cekStorage/Logos/{org.logo}"
-                        heading={org.name}
-                        link="/organizers/{org.slug}"
-                    />
-                {/each}
-            </div>
+
+<Heading tag="h2" class="mb-4 text-center">Completed Events</Heading>
+<div class="flex flex-row flex-wrap justify-center items-center gap-10 align-baseline lg:grid lg:grid-cols-3">
+	{#each organizers as org}
+		<div class="flex flex-col justify-center items-center gap-5">
+			<Avatar
+				src="https://xnpuqvymwwzjiwycsabx.supabase.co/storage/v1/object/public/cekStorage/Logos/{org.logo}" class="size-36 hover:animate-pulse" border href="/organizers/{org.slug}"/>
+                <P class="">{org.name}</P>
+		</div>
+	{/each}
+</div>
