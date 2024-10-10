9a2e91f167701e726e493e4eccdafa4eb225be2c
vachan-maker <vachan@tutanota.com>
2024-10-10 16:31:20 +0530

    fix events not appearing because of case sensitivity

--- src/routes/organizers/[slug]/+page.server.ts
+++ src/routes/organizers/[slug]/+page.server.ts
@@ -4,6 +4,7 @@ let today:string = new Date().toISOString().slice(0,10);
 console.log(today);
 export async function load({params}) {
     const {slug} = params;
+    console.log({slug});
     const { data } = await supabase.from("Organizers").select('*').ilike('slug', slug);
     const events =  (await supabase.from("Upcoming Events").select('*').contains('tags', [slug]).order('date',{ascending:true})).data;
     console.log({data});
